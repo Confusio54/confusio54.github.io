da57ac4b9e8dd3efe060cf196a49188333eff32d
Confusio54 <56383203+Confusio54@users.noreply.github.com>
2026-01-10 01:01:17 +0000

    Deploying to gh-pages from @ Confusio54/confusio54.github.io@00f6aadf1c88339ab08d022846e0b0e59cc3c84d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Confusio54/ymwang.github.io",
+    "data-repo": "Confusio54/confusio54.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
